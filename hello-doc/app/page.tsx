02f9bf4e2cc814525d4319fa15c67ddd050ad182
Ahmedbo9 <ahmed.benothman@live.fr>
2024-07-21 00:48:05 -0400

    adding styling

--- hello-doc/app/page.tsx
+++ hello-doc/app/page.tsx
@@ -1,9 +1,34 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import PatientForm from '../components/forms/PatientForm';
+import Link from 'next/link';
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline text-white">Hello, world!</h1>
-      <Button>hi</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[469]">
+        <Image src= "/assets/icons/logo-full.svg"
+        height={1000}
+        width={1000}
+        className="mb-12 h-10 w-fit"
+         alt="home-img">
+         </Image>
+         <PatientForm/>
+         <div className="text-14-regular mt-20 justify-between flex">
+          <p className="justify-items-end text-dark-600 xl:text-left"> © 2021 HelloDoc. All rights reserved.</p>
+          <Link href="" className="text-green-500">
+          Admin
+          </Link>
+         </div>
+      </div>
+      </section>
+      <Image
+      src= "/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      className="side-img max-w-[50%]"
+      alt="home-img"
+      >
+      </Image>
     </div>
   );
 }
